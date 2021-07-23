6907331ba58a4061cec5a5848ebac5071e2ce6f0
hyeoneedyou <hyeoneedyou@naver.com>
2021-07-23 22:23:29 +0900

    feat: Add level field to Profile model

--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=10)
     image = models.ImageField(upload_to='profile_image/', blank=True, null=True)
+    level = models.IntegerField(default=1)
